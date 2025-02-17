2b0ea163ac7579379b38faa99ca338610540a62b
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2025-02-17 11:05:39 -0300

    Add files via upload

---
+++ aula_02_17-02-2025/exercicios/exercicio02.php
@@ -0,0 +1,12 @@
+<?php
+$num = 7;
+
+if ($num < 0) {
+    echo "o número é negativo";
+} elseif ($num > 0) {
+    echo "o número é positivo";
+} else ($num = 0) {
+    echo "o número é igual a zero";
+}
+
+?>
